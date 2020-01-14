76da2cb611f29b08c2fce6672c4ea9bb27d1659e
123lenah <123lenah@gmail.com>
2020-01-14 11:24:58 -0800

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Lenah Syed on 1/14/20.
+//  Copyright © 2020 lenahsapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
